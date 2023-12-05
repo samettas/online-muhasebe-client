0c17e75a15ee8f0c4834926f954ada79c8954b00
Samet Taş <bmsamettas@gmail.com>
2023-12-05 17:41:32 +0300

    Create UCAF Plan

--- src/app/ui/components/ucafs/services/ucaf.service.ts
+++ src/app/ui/components/ucafs/services/ucaf.service.ts
@@ -4,6 +4,7 @@ import { UcafModel } from '../models/ucaf.model';
 import { LoginResponseModel } from '../../auth/models/login-response.model';
 import { CryptoService } from 'src/app/common/services/crypto.service';
 import { ResponseModel } from 'src/app/common/models/response.model';
+import { MessageResponseModel } from 'src/app/common/models/message-response.model';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +24,10 @@ export class UcafService {
   getAll(callBack: (res:ResponseModel<UcafModel[]>)=> void) {
     let model = {companyId: this.loginResponse.company.companyId};
     this._http.post<ResponseModel<UcafModel[]>>("UCAFs/GetAllUCAF", model ,res=> callBack(res));
+  }
+
+  add(model:UcafModel, callBack: (res: MessageResponseModel)=> void) {
+    model.companyId = this.loginResponse.company.companyId;
+    this._http.post<MessageResponseModel>("UCAFs/CreateUCAF", model, (res)=> callBack(res));
   }
 }
--- src/app/ui/components/ucafs/ucafs.component.ts
+++ src/app/ui/components/ucafs/ucafs.component.ts
@@ -6,12 +6,22 @@ import { NavModel } from 'src/app/common/components/blank/models/navs.model';
 import { UcafService } from './services/ucaf.service';
 import { UcafModel } from './models/ucaf.model';
 import { UcafPipe } from './pipes/ucaf.pipe';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ValidInputDirective } from 'src/app/common/directives/valid-input.directive';
+import { LoadingButtonComponent } from 'src/app/common/components/loading-button/loading-button.component';
+import { ToastrService, ToastrType } from 'src/app/common/services/toastr.service';
 
 @Component({
   selector: 'app-ucafs',
   standalone: true,
-  imports: [CommonModule, BlankComponent, SectionComponent, UcafPipe, FormsModule],
+  imports: [CommonModule, 
+    BlankComponent, 
+    SectionComponent, 
+    UcafPipe, 
+    FormsModule, 
+    ValidInputDirective, 
+    FormsModule,
+  LoadingButtonComponent],
   templateUrl: './ucafs.component.html',
   styleUrls: ['./ucafs.component.css'],
 })
@@ -28,13 +38,15 @@ export class UcafsComponent implements OnInit{
       name: 'Hesap Planı',
     },
   ];
-
   ucafs: UcafModel[] = [];
-
   filterText:string = "";
+  isAddForm: boolean = false;
+  ucafType: string = "M";
+  isLoading: boolean = false;
 
   constructor(
-    private _ucaf: UcafService
+    private _ucaf: UcafService,
+    private _toastr: ToastrService
   ) {}
 
   ngOnInit(): void {
@@ -43,5 +55,27 @@ export class UcafsComponent implements OnInit{
 
   getAll() {
     this._ucaf.getAll(res=> {this.ucafs = res.data;});
+  }
+
+  showAddForm() {
+    this.isAddForm = true;
+  }
+
+  add(form: NgForm) {
+    if(form.valid) {
+      this.isLoading = true;
+      let model = new UcafModel();
+      model.code = form.controls["code"].value;
+      model.type = form.controls["type"].value;
+      model.name = form.controls["name"].value;
+
+      this._ucaf.add(model, (res)=> {
+        form.reset();
+        this.ucafType = "M";
+        this.getAll();
+        this.isLoading = false;
+        this._toastr.toast(ToastrType.Succes, "Başarılı!",res.message);
+      });
+    }
   }
 }
